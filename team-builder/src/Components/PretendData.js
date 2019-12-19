2f09b653f2fa6c807e97a7c8b2dd5c511b94d595
Dani Blackwell <bleuxgenius@gmail.com>
2019-12-19 15:44:00 -0600

    Steps 1 & 2

---
+++ team-builder/src/Components/PretendData.js
@@ -0,0 +1,15 @@
+const PretendData = [ 
+    {
+        id: 1,
+        name: 'Dani',
+        email: 'danifrontend@fluiddev.com',
+        role: 'Front-end'
+    },
+    {
+        id: 2,
+        name: 'Kyle',
+        email: 'kyledesigner@fluiddev.com',
+        role: 'Designer'
+    },
+
+]
